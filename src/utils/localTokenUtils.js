048acc44c4bc7975f5c09f2f205be374b40dfce1
yeongi <cbg5255@gmail.com>
2023-04-26 23:27:40 +0900

    feat : 검증 및 localstorage 토큰 저장 유틸 구현

---
+++ src/utils/localTokenUtils.js
@@ -0,0 +1,15 @@
+const { localStorage } = window;
+
+const tokenKey = 'accessToken';
+
+export const putUserTokenInLocalStorage = token => {
+  localStorage.setItem(tokenKey, token);
+};
+
+export const getUserTokenInLocalStorage = () => {
+  localStorage.getItem(tokenKey);
+};
+
+export const removeUserTokenInLocalStorage = () => {
+  localStorage.removeItem(tokenKey);
+};
