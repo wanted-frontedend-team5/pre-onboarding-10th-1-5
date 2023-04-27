0a4952016d59083c219d5af298475f1275c74f5d
doctork4 <doctor.k1994@gmail.com>
2023-04-28 00:03:43 +0900

    feat : Todo CRUD 기능 구현

---
+++ src/components/todo/ToDoUnit.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import Button from '../Button';
+import Input from '../Input';
+import { axiosAuthInstance } from './axiosInstance';
+
+export default function ToDoUnit({ todo, deleteToDo, setCheck, getToDos }) {
+  const [editMode, setEditMode] = useState(false);
+  const [newToDo, setNewToDo] = useState(todo.todo);
+
+  const editToDo = async () => {
+    await axiosAuthInstance.put(`./todos/${todo.id}`, {
+      todo: newToDo,
+      isCompleted: todo.isCompleted,
+    });
+    // console.log(response);
+    getToDos();
+    setEditMode(false);
+  };
+
+  // 취소 버튼 이벤트 핸들러
+  const cancelEdit = () => {
+    setEditMode(false);
+    setNewToDo(todo.todo);
+  };
+
+  return (
+    <li key={todo.id}>
+      {!editMode ? (
+        <label htmlFor="toDoCheck">
+          <Input
+            id="toDoCheck"
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={() => setCheck(todo)}
+          />
+          <span>{todo.todo}</span>
+          <Button
+            data-testid="modify-button"
+            type="button"
+            onClick={() => setEditMode(true)}
+          >
+            수정
+          </Button>
+          <Button
+            data-testid="delete-button"
+            type="button"
+            onClick={() => deleteToDo(todo)}
+          >
+            삭제
+          </Button>
+        </label>
+      ) : (
+        <form key={todo.id} onSubmit={editToDo} id="editForm">
+          <Input
+            data-testid="modify-input"
+            value={newToDo}
+            form="editForm"
+            onChange={e => setNewToDo(e.target.value)}
+          />
+          <Button data-testid="submit-button" type="submit">
+            제출
+          </Button>
+          <Button
+            data-testid="cancel-button"
+            type="button"
+            onClick={cancelEdit}
+          >
+            취소
+          </Button>
+        </form>
+      )}
+    </li>
+  );
+}
--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,5 +1,79 @@
+import { useState, useEffect } from 'react';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import ToDoUnit from '../../components/todo/todounit';
+import { axiosAuthInstance } from './axiosInstance';
+
 function Todo() {
-  return <h1>이것들은 내가 할 일이지...</h1>;
+  const token = localStorage.getItem('token');
+  // 상태 선언
+  const [inputValue, setInputValue] = useState('');
+  const [toDos, setToDos] = useState([]);
+
+  // 서버에서 todo data를 받아와 toDos에 갱신
+  const getToDos = async () => {
+    const response = await axiosAuthInstance.get('./todos');
+    setToDos(response.data);
+    // console.log(response);
+  };
+  // 새로 입력받은 todo를 서버에 post, 화면의 toDos 갱신
+  const addToDo = async () => {
+    const createNewToDo = {
+      todo: inputValue,
+      isCompleted: false,
+    };
+    await axiosAuthInstance.post('./todos', createNewToDo);
+    getToDos();
+    // console.log(response);
+  };
+
+  // todo 삭제
+  const deleteToDo = async todo => {
+    await axiosAuthInstance.delete(`./todos/${todo.id}`);
+    // console.log(response);
+    getToDos();
+  };
+
+  // 체크박스 변경
+  const setCheck = async todo => {
+    await axiosAuthInstance.put(`./todos/${todo.id}`, {
+      todo: todo.todo,
+      isCompleted: !todo.isCompleted,
+    });
+    getToDos();
+  };
+
+  // Side Effect
+  useEffect(() => {
+    if (token) getToDos();
+  }, [token]);
+
+  return (
+    <>
+      <form onSubmit={addToDo}>
+        <Input
+          data-testid="new-todo-input"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <Button data-testid="new-todo-add-button" type="submit">
+          추가
+        </Button>
+      </form>
+
+      <ul>
+        {toDos.map(todo => (
+          <ToDoUnit
+            key={todo.id}
+            todo={todo}
+            deleteToDo={deleteToDo}
+            setCheck={setCheck}
+            getTodos={getToDos}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Todo;
