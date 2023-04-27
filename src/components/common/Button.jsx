277a81a816c7814623dd08c41ab6c3b594dc6780
khv2644511 <khv2644511@naver.com>
2023-04-27 10:15:41 +0900

    feat: Button 컴포넌트 타입 속성 지정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({ children, bgcolor, txtcolor }) {
   return (
     <button
       className={`${colorVariants[bgcolor]} ${colorVariants[txtcolor]} py-2 px-4 my-4 rounded-lg hover:scale-105`}
-      type="button"
+      type="submit"
     >
       {children}
     </button>
