3024349ed94eb96c8adbb92a7ef2c0e71de6b21e
khv2644511 <khv2644511@naver.com>
2023-04-28 02:37:09 +0900

    feat: Todo 페이지 스타일 및 조회 기능 추가

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,5 +1,27 @@
-function Todo() {
-  return <h1>이것들은 내가 할 일이지...</h1>;
-}
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import LogoutButton from '../../components/todo/LogoutButton';
+import TodoInput from '../../components/todo/TodoInput';
+import TodoList from '../../components/todo/TodoList';
+import useGetTodo from '../../hooks/useGetTodo';
 
-export default Todo;
+export default function Todo() {
+  const [isLoading, todos, getTodos] = useGetTodo();
+
+  useEffect(() => {
+    getTodos();
+  }, [getTodos]);
+
+  return (
+    <main className="bg-pink-200 flex flex-col place-items-center container w-96 px-6 py-6 rounded-xl">
+      <header className="flex flex-row gap-3">
+        <h2>Todo List</h2>
+        <LogoutButton />
+      </header>
+      <section>
+        <TodoInput getTodos={getTodos} />
+        <TodoList todos={todos} isLoading={isLoading} getTodos={getTodos} />
+      </section>
+    </main>
+  );
+}
