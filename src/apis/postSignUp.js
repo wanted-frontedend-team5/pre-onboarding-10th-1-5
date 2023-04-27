d9be020a1d2c988d58708eebef617b6f093af125
codnr <wnd75913@gmail.com>
2023-04-27 18:38:44 +0900

    delete: 임시로 만든 apis 폴더 삭제

--- src/apis/postSignUp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://www.pre-onboarding-selection-task.shop';
-const postSignUp = async pathData => {
-  await axios.post(`${API_URL}/auth/signup`, pathData);
-};
-
-export default postSignUp;
