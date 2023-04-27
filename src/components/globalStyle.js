a32436259774d228461d2aa94ca49fe99f9cbc38
codnr <wnd75913@gmail.com>
2023-04-27 10:19:53 +0900

    design: globlaStyle 생성

---
+++ src/components/globalStyle.js
@@ -0,0 +1,10 @@
+export const inputStyle =
+  'py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent';
+
+export const buttonStyle =
+  'border p-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 disabled:bg-blue-300';
+
+export const smButtonStyle = 'mx-0.5 text-gray-500 text-sm hover:text-gray-700';
+
+export const smInputStyle =
+  'px-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent';
