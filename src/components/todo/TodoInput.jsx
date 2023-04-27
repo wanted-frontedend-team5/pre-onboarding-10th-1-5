9fccbade541107f9a6cc6fe4f26b8ea0472c3765
khv2644511 <khv2644511@naver.com>
2023-04-28 01:53:50 +0900

    feat: TodoInput 컴포넌트 생성

---
+++ src/components/todo/TodoInput.jsx
@@ -0,0 +1,37 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+import useInput from '../../hooks/useInput';
+import Input from '../Input';
+import Button from '../Button';
+import todoApi from '../../api/todo';
+
+export default function TodoInput({ getTodos }) {
+  const [{ todo }, onChange, setValue] = useInput({ todo: '' });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if (!todo) {
+      return alert('투두를 입력해주세요.');
+    }
+
+    await todoApi.createTodo(todo);
+    getTodos();
+    setValue('todo', '');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-row">
+      <Input
+        onChange={onChange}
+        type="text"
+        data-testid="new-todo-input"
+        placeholder="할 일을 입력하세요."
+        value={todo}
+        name="todo"
+      />
+      <Button data-testid="new-todo-add-button">
+        <AiOutlinePlus />
+      </Button>
+    </form>
+  );
+}
