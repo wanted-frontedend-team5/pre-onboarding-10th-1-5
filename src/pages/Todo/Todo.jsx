3f3655f597459a692c06223a54ce489e9c008b5c
codnr <wnd75913@gmail.com>
2023-04-28 09:20:53 +0900

    feat: Todo page 생성

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,5 +1,50 @@
-function Todo() {
-  return <h1>이것들은 내가 할 일이지...</h1>;
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import getToDoList from '../apis/getToDoList';
+import ToDoForm from '../components/ToDoForm';
+import ToDo from '../components/ToDo';
+
+function ToDoList() {
+  const [toDos, setToDos] = useState([]);
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('access_token');
+
+  useEffect(() => {
+    const patchToDoList = async () => {
+      try {
+        const response = await getToDoList(accessToken);
+        setToDos(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (!accessToken) {
+      navigate('/signin');
+    }
+
+    patchToDoList();
+  }, [accessToken, navigate]);
+
+  return (
+    <div className="space-y-1 w-full">
+      <h1 className="text-3xl">ToDo List</h1>
+      <ToDoForm toDos={toDos} setToDos={setToDos} accessToken={accessToken} />
+      <ul className="flex flex-col">
+        {toDos.map(todo => (
+          <ToDo
+            key={todo.id}
+            id={todo.id}
+            todo={todo.todo}
+            isCompleted={todo.isCompleted}
+            toDos={toDos}
+            setToDos={setToDos}
+            accessToken={accessToken}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Todo;
+export default ToDoList;
