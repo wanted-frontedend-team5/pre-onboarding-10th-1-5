7511a9f7f2e8948df0c42a52295028a9c6c3c704
yeongi <cbg5255@gmail.com>
2023-04-28 20:21:20 +0900

    feat : todo 수정 및 컴포넌트 작성

---
+++ src/components/todo/ModifyTodoForm.jsx
@@ -0,0 +1,47 @@
+import todoApi from 'api/todo';
+import DefaultButton from 'components/button/DefaultButton';
+import SubmitButton from 'components/button/SubmitButton';
+import Input from 'components/input/Input';
+import useInput from 'hooks/useInput';
+
+function ModifyTodoForm({ todo, refreshHandler, closeHandler }) {
+  const [{ modifyTodo }, onChange, setValue] = useInput({ modifyTodo: '' });
+
+  const todoSubmitHandler = async e => {
+    e.preventDefault();
+
+    if (modifyTodo.trim().length === 0) return;
+    const info = {
+      id: todo.id,
+      todo: modifyTodo,
+      isCompleted: todo.isCompleted,
+    };
+
+    await todoApi.updateTodo(info);
+
+    setValue('setValue', '');
+
+    refreshHandler();
+    closeHandler();
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <Input
+        type="text"
+        label="수정할 Todo 작성"
+        dataTestid="modify-input"
+        name={modifyTodo}
+        id="modifyTodo"
+        value={modifyTodo}
+        onChange={onChange}
+      />
+      <SubmitButton data-testid="submit-button">추가</SubmitButton>
+      <DefaultButton data-testid="cancel-button" onClick={closeHandler}>
+        취소
+      </DefaultButton>
+    </form>
+  );
+}
+
+export default ModifyTodoForm;
